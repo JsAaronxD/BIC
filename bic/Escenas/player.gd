7baeb2cf404b988d8dd6e17719319d8a84955abb
Aarón Zavala <aaronzavalagonzalez334@gmail.com>
2025-09-17 08:48:36 -0700

    no se

--- bic/Escenas/player.gd
+++ bic/Escenas/player.gd
@@ -1,23 +1,20 @@
 extends CharacterBody2D
 
-@export var speed = 150  # Velocidad del personaje
+@export var speed = 550 
 @onready var animated_sprite = $AnimatedSprite2D
 
-# Guardamos la última animación usada
 var last_animation : String = "walk_down"
 
 func _physics_process(delta):
 	var direction = Vector2.ZERO
 	var is_moving = false
 
-	# Movimiento vertical tiene prioridad
 	if Input.is_action_pressed("ui_up"):
 		direction.y = -1
 		is_moving = true
 	elif Input.is_action_pressed("ui_down"):
 		direction.y = 1
 		is_moving = true
-	# Solo si no hay movimiento vertical, chequea horizontal
 	elif Input.is_action_pressed("ui_left"):
 		direction.x = -1
 		is_moving = true
@@ -28,7 +25,6 @@ func _physics_process(delta):
 	if is_moving:
 		velocity = direction * speed
 
-		# Decide animación según dirección y la guarda como última
 		if direction.y < 0:
 			last_animation = "walk_up"
 		elif direction.y > 0:
@@ -41,7 +37,6 @@ func _physics_process(delta):
 		animated_sprite.play(last_animation)
 	else:
 		velocity = Vector2.ZERO
-		# Reproduce la última animación, pero se queda en el primer frame (quieto)
 		animated_sprite.play(last_animation)
 		animated_sprite.stop()
 
