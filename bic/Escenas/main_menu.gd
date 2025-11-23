54c5ca50ba05962530816551c3ba690f9784f74b
Aarón Zavala <aaronzavalagonzalez334@gmail.com>
2025-11-23 01:15:52 -0700

    prueba de cambios para un sistema de niveles

--- bic/Escenas/main_menu.gd
+++ bic/Escenas/main_menu.gd
@@ -6,8 +6,9 @@ func _on_play_pressed():
 	if game_scene:
 		get_tree().change_scene_to_packed(game_scene)
 
-func _on_options_pressed():
-	print("Abrir menú de opciones")
-
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_niveles_pressed() -> void:
+	pass # Replace with function body.
