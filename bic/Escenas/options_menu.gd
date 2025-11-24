116fbfdf3308fbc2c4b448de30e386c5ee3d3d1e
Aarón Zavala <aaronzavalagonzalez334@gmail.com>
2025-11-23 18:31:16 -0700

    cambios a la estetica del slider del menu de opciones

--- bic/Escenas/options_menu.gd
+++ bic/Escenas/options_menu.gd
@@ -4,19 +4,16 @@ extends CanvasLayer
 @onready var sfx_slider = $VBoxContainer/SFXSlider
 
 func _ready():
-	# Conecta las señales de los sliders y el botón
 	music_slider.value_changed.connect(_on_music_slider_value_changed)
 	sfx_slider.value_changed.connect(_on_sfx_slider_value_changed)
 	$BackButton.pressed.connect(_on_back_button_pressed)
 	
-	# (Opcional) Pon los sliders en la posición correcta actual
 	music_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music")))
 	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))
 
 func _on_music_slider_value_changed(value):
-	# Busca el índice del bus "Music"
 	var bus_index = AudioServer.get_bus_index("Music")
-	# Convierte el valor lineal (0 a 1) a Decibelios y lo aplica
+	# Convertimos el valor lineal a decibelios y lo aplica
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 
 func _on_sfx_slider_value_changed(value):
